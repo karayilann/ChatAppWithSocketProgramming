d27d726e3f782442c0d9fe46bc2134dcd18923b4
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2024-10-06 00:33:31 +0300

    Initial

---
+++ ChatApplication/ServerForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ChatApplication
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSendFile = new Button();
+            txtFilePath = new TextBox();
+            txtMessage = new TextBox();
+            btnSendMessage = new Button();
+            rTextBoxOldMessages = new RichTextBox();
+            btnListen = new Button();
+            txtIpAndPort = new TextBox();
+            SuspendLayout();
+            // 
+            // btnSendFile
+            // 
+            btnSendFile.Location = new Point(464, 422);
+            btnSendFile.Name = "btnSendFile";
+            btnSendFile.Size = new Size(75, 22);
+            btnSendFile.TabIndex = 0;
+            btnSendFile.Text = "Send File";
+            btnSendFile.UseVisualStyleBackColor = true;
+            btnSendFile.Click += btnSendFile_Click;
+            // 
+            // txtFilePath
+            // 
+            txtFilePath.Location = new Point(464, 393);
+            txtFilePath.Name = "txtFilePath";
+            txtFilePath.PlaceholderText = "File Path";
+            txtFilePath.ReadOnly = true;
+            txtFilePath.Size = new Size(277, 23);
+            txtFilePath.TabIndex = 1;
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(3, 422);
+            txtMessage.Name = "txtMessage";
+            txtMessage.PlaceholderText = "Enter Your Message";
+            txtMessage.Size = new Size(277, 23);
+            txtMessage.TabIndex = 3;
+            // 
+            // btnSendMessage
+            // 
+            btnSendMessage.Location = new Point(286, 422);
+            btnSendMessage.Name = "btnSendMessage";
+            btnSendMessage.Size = new Size(131, 23);
+            btnSendMessage.TabIndex = 5;
+            btnSendMessage.Text = "Send Message";
+            btnSendMessage.UseVisualStyleBackColor = true;
+            btnSendMessage.Click += btnSendMessage_Click;
+            // 
+            // rTextBoxOldMessages
+            // 
+            rTextBoxOldMessages.Location = new Point(3, 138);
+            rTextBoxOldMessages.Name = "rTextBoxOldMessages";
+            rTextBoxOldMessages.ReadOnly = true;
+            rTextBoxOldMessages.Size = new Size(455, 278);
+            rTextBoxOldMessages.TabIndex = 11;
+            rTextBoxOldMessages.Text = "";
+            // 
+            // btnListen
+            // 
+            btnListen.Location = new Point(173, 12);
+            btnListen.Name = "btnListen";
+            btnListen.Size = new Size(75, 23);
+            btnListen.TabIndex = 14;
+            btnListen.Text = "Listen";
+            btnListen.UseVisualStyleBackColor = true;
+            btnListen.Click += btnListen_Click;
+            // 
+            // txtIpAndPort
+            // 
+            txtIpAndPort.Location = new Point(12, 12);
+            txtIpAndPort.Name = "txtIpAndPort";
+            txtIpAndPort.PlaceholderText = "IP,Port";
+            txtIpAndPort.Size = new Size(155, 23);
+            txtIpAndPort.TabIndex = 13;
+            // 
+            // ServerForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnListen);
+            Controls.Add(txtIpAndPort);
+            Controls.Add(btnSendFile);
+            Controls.Add(rTextBoxOldMessages);
+            Controls.Add(btnSendMessage);
+            Controls.Add(txtMessage);
+            Controls.Add(txtFilePath);
+            Name = "ServerForm";
+            Text = "ServerForm";
+            FormClosing += ServerForm_FormClosing;
+            Load += ServerForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSendFile;
+        private TextBox txtFilePath;
+        private TextBox txtMessage;
+        private Button btnSendMessage;
+        private RichTextBox rTextBoxOldMessages;
+        private Button btnListen;
+        private TextBox txtIpAndPort;
+    }
+}
---
+++ ChatApplication/ServerForm.cs
@@ -0,0 +1,147 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace ChatApplication
+{
+    public partial class ServerForm : Form
+    {
+        private NetworkStream ns;
+        private TcpClient client;
+        public ServerForm()
+        {
+            InitializeComponent();
+            btnSendFile.Enabled = false;
+            btnSendMessage.Enabled = false;
+        }
+
+        private void ServerForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void StartServer(string ip,int port)
+        {
+            try
+            {
+                TcpListener server = new TcpListener(IPAddress.Parse(ip), port);
+                server.Start();
+
+                client = server.AcceptTcpClient();
+                ns = client.GetStream();
+                btnSendMessage.Enabled = true;
+
+                Thread receiveThread = new Thread(ReceiveMessages);
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Start server Hata: " + ex.Message);
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                StringBuilder completeMessage = new StringBuilder();
+
+                while ((bytesRead = ns.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    string receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    completeMessage.Append(receivedMessage);
+                    if (receivedMessage.StartsWith("File"))
+                    {
+                        btnSendFile.Enabled = true;
+                    }
+                    else
+                    {
+                        Invoke((MethodInvoker)delegate
+                        {
+                            AddToOldMessages("Ýstemci : " + completeMessage.ToString());
+
+                        });
+                        completeMessage.Clear();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata (Mesaj alýrken): " + ex.Message);
+            }
+        }
+
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(txtMessage.Text)) return;
+                string message = txtMessage.Text;
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                ns.Write(data, 0, data.Length);
+                AddToOldMessages("Sunucu : " + message);
+                txtMessage.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata (Mesaj gönderirken): " + ex.Message);
+            }
+        }
+
+        private void ServerForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ns?.Close();
+            client?.Close();
+        }
+
+        private void btnSendFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = ofd.FileName;
+                    FileInfo fileInfo = new FileInfo(fileName);
+                    long fileSize = fileInfo.Length;
+
+                    byte[] fileNameBytes = Encoding.ASCII.GetBytes("FILE:" + Path.GetFileName(fileName) + "|SIZE:" + fileSize.ToString() + "|");
+                    ns.Write(fileNameBytes, 0, fileNameBytes.Length);
+                    ns.Flush();
+
+                    byte[] fileData = File.ReadAllBytes(fileName);
+                    ns.Write(fileData, 0, fileData.Length);
+                    ns.Flush();
+
+                    MessageBox.Show("Dosya gönderildi.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata (Dosya gönderirken): " + ex.Message);
+            }
+        }
+
+
+        private void AddToOldMessages(string message)
+        {
+            rTextBoxOldMessages.Text += message + Environment.NewLine;
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            string[] strings = txtIpAndPort.Text.Split(',');
+            string ip = strings[0];
+            int port = int.Parse(strings[1]);
+            Thread serverThread = new Thread(() => StartServer(ip, port));
+            serverThread.IsBackground = true;
+            serverThread.Start();
+
+        }
+    }
+}
